1c3dc3825b3deede9f17451470a5335b674ad691
Arthur Cavalcanti <arthurrcavalcanti@gmail.com>
2025-04-25 12:30:57 -0300

    melhorando sitema de carregamento de descrição dos livros automáticamente

--- frontend/ebooksreader/src/views/AdicionarLivros.vue
+++ frontend/ebooksreader/src/views/AdicionarLivros.vue
@@ -1,6 +1,6 @@
 <script>
 // api.js é onde você encontra as chamadas para as rotas definidas em server.js no backend.
-import api from "../services/api.js";
+import api from '../services/api.js'
 
 export default {
   props: [],
@@ -17,97 +17,100 @@ export default {
     //exemplo de uso do watch de uma propriedade
     isbn(novo, velho) {
       // se novo == velho, não faz nada
-      if(novo === velho) return;
+      if (novo === velho) return
 
-      console.log("ISBN alterado de", velho, "para", novo);
-      if(novo.length > 7){
-        this.buscarLivroISBN(novo);
+      console.log('ISBN alterado de', velho, 'para', novo)
+      if (novo.length > 7) {
+        this.buscarLivroISBN(novo)
       }
-    }
+    },
   },
-  computed: {
-  },
+  computed: {},
   methods: {
     async buscarLivroISBN(valor) {
       // api de consulta da openlibrary
       // https://openlibrary.org/dev/docs/api/books
-      const url = `https://openlibrary.org/isbn/${valor}.json`;
-      const response = await fetch(url);
-      
-      if(!response.ok){
-        console.log("Erro ao buscar livro:", { response });
-        return;
+      const url = `https://openlibrary.org/isbn/${valor}.json`
+      const response = await fetch(url)
+      let authorKey = undefined
+      if (!response.ok) {
+        console.log('Erro ao buscar livro:', { response })
+        return
       }
 
       // verifica se a resposta é ok (status 200)
-      if(response.ok) {
-        const data = await response.json();
-        console.log("Livro encontrado:", { data });
+      if (response.ok) {
+        const data = await response.json()
+        console.log('Livro encontrado:', { data })
 
-        if(data.description){
-          this.description = data.description;
+        if (data.description) {
+          this.description = data.description
         } else {
-          if(data.works) {
-            const work = data.works[0].key;
-            const urlWork = `https://openlibrary.org${work}.json`;
-            const responseWork = await fetch(urlWork);
-            if(responseWork.ok) {
-              const dataWork = await responseWork.json();
-              console.log("Work encontrado:", { dataWork });
-              this.description = dataWork.description;
+          if (data.works) {
+            const work = data.works[0].key
+            const urlWork = `https://openlibrary.org${work}.json`
+            const responseWork = await fetch(urlWork)
+            if (responseWork.ok) {
+              const dataWork = await responseWork.json()
+              console.log('Work encontrado:', { dataWork })
+              authorKey = dataWork.authors?.[0]?.author?.key
+              if (dataWork.description.value) {
+                this.description = dataWork.description.value
+              } else {
+                this.description = dataWork.description
+              }
             } else {
-              console.log("Erro ao buscar work:", { responseWork });
+              console.log('Erro ao buscar work:', { responseWork })
             }
-          } 
+          }
         }
-        this.title = data.title;
+        this.title = data.title
 
-        this.cover = `https://covers.openlibrary.org/b/id/${data.covers?.[0]}-M.jpg`; // -M é médio, -S é pequeno, -L é grande
+        this.cover = `https://covers.openlibrary.org/b/id/${data.covers?.[0]}-M.jpg` // -M é médio, -S é pequeno, -L é grande
 
-        const author = data.authors?.[0]?.key;
+        const author = data.authors?.[0]?.key ?? authorKey
         // se não encontrar o autor, tenta pegar o primeiro autor da lista de colaboradores
-        if(!author){
-          const contributors = data.contributors?.find(c => c.role == 'Author')?.name;
-          if(contributors) {
-            this.author = contributors;
+        if (!author) {
+          const contributors = data.contributors?.find((c) => c.role == 'Author')?.name
+          if (contributors) {
+            this.author = contributors
           }
         }
 
-        if(author) {
-          const urlAuthor = `https://openlibrary.org${author}.json`;
+        if (author) {
+          const urlAuthor = `https://openlibrary.org${author}.json`
 
-          const responseAuthor = await fetch(urlAuthor);
-          if(responseAuthor.ok) {
-            const dataAuthor = await responseAuthor.json();
-            console.log("Author Author:", { dataAuthor });
-            this.author = dataAuthor.name;
+          const responseAuthor = await fetch(urlAuthor)
+          if (responseAuthor.ok) {
+            const dataAuthor = await responseAuthor.json()
+            console.log('Author Author:', { dataAuthor })
+            this.author = dataAuthor.name
           }
         }
       }
-      
     },
     async adicionarLivro() {
-      const { isbn, title, author, description, cover } = this;
+      const { isbn, title, author, description, cover } = this
       const livro = {
         isbn,
         title,
         author,
         description,
-        cover
+        cover,
       }
 
-      if(!isbn || !title || !author || !description || !cover) {
-        alert("Preencha todos os campos");
-        return;
+      if (!isbn || !title || !author || !description || !cover) {
+        alert('Preencha todos os campos')
+        return
       }
 
-      console.log("Adicionando livro:", {livro});
-      api.adicionarLivro(livro);
-    }
+      console.log('Adicionando livro:', { livro })
+      api.adicionarLivro(livro)
+    },
   },
   mounted() {
-    console.log("Adicionar livros montado");
-  }
+    console.log('Adicionar livros montado')
+  },
 }
 </script>
 
@@ -115,12 +118,18 @@ export default {
   <header>
     <h1>Você pode adicionar livros aqui</h1>
     <p>Preencha as informações do livro abaixo</p>
-    <p>Não sabe o ISBN do livro procure em <a href="https://openlibrary.org/" target="_blank">https://openlibrary.org/</a></p>
+    <p>
+      Não sabe o ISBN do livro procure em
+      <a href="https://openlibrary.org/" target="_blank">https://openlibrary.org/</a>
+    </p>
   </header>
   <main>
     <form v-on:submit.prevent="adicionarLivro" class="adding-form">
       <!-- icone da fontawesome -->
-      <label for="isbn">Código ISBN <i class="fa fa-info-circle" title="Pesquisa automaticamente na api da openlibrary"></i></label>
+      <label for="isbn"
+        >Código ISBN
+        <i class="fa fa-info-circle" title="Pesquisa automaticamente na api da openlibrary"></i
+      ></label>
       <input type="text" id="isbn" v-model="isbn" placeholder="Digite o código ISBN do livro" />
 
       <label for="title">Título</label>
@@ -135,15 +144,12 @@ export default {
       <label for="cover">Capa</label>
       <input type="text" id="cover" v-model="cover" placeholder="Digite a url para capa do livro" />
 
-
       <button type="submit">Adicionar</button>
-
     </form>
   </main>
 </template>
 
 <style>
-
 .adding-form {
   display: flex;
   flex-direction: column;
@@ -154,14 +160,13 @@ export default {
     font-weight: 700;
     margin-block-end: -0.5em;
   }
-  
-  
+
   textarea {
     height: 10em;
   }
 
   button {
-    padding:.5em 1em;
+    padding: 0.5em 1em;
   }
   padding-block-end: 1em;
   border-block-end: 1px solid #ccc;
--- frontend/ebooksreader/src/views/HomeView.vue
+++ frontend/ebooksreader/src/views/HomeView.vue
@@ -1,6 +1,6 @@
 <script>
 // api.js é onde você encontra as chamadas para as rotas definidas em server.js no backend.
-import api from "../services/api.js";
+import api from '../services/api.js'
 
 export default {
   props: [],
@@ -8,56 +8,58 @@ export default {
     return {
       // aqui você pode criar variáveis que podem ser usadas no template
       livros: [],
-      pesquisa: undefined
+      pesquisa: undefined,
     }
   },
   watch: {
     // aqui você pode observar mudanças em variáveis
     pesquisa(novo, velho) {
       // esse método é chamado sempre que a pesquisa muda
-      if(novo !== velho){
+      if (novo !== velho) {
         this.buscarLivro()
       }
-    }
+    },
   },
   computed: {
     // aqui você pode criar variáveis que dependem de outras
     // por exemplo, você pode criar uma variável que retorna o número de livros
     // esse exemplo é usado no H2 do html abaixo
     numeroLivros() {
-      return this.livros.length;
-    }
+      return this.livros.length
+    },
+    descricaoLivro(livro) {
+      return livro.description.length > 100
+        ? livro.description.substring(0, 100) + '...'
+        : livro.description
+    },
   },
   methods: {
     // aqui você pode criar métodos que podem ser chamados em qualquer lugar
     async listarLivros() {
-      const livros = await api.listarLivros();
-      console.log("Livros:", {livros});
-      this.livros = livros;
+      const livros = await api.listarLivros()
+      console.log('Livros:', { livros })
+      this.livros = livros
     },
-    async buscarLivro(){
-      const {pesquisa} = this;
-      const livros = await api.buscarLivros(pesquisa);
-      console.log("Livros encontrados:", {livros});
-      this.livros = livros;
-    }
+    async buscarLivro() {
+      const { pesquisa } = this
+      const livros = await api.buscarLivros(pesquisa)
+      console.log('Livros encontrados:', { livros })
+      this.livros = livros
+    },
   },
   mounted() {
     // esse método é chamado assim que o componente é montado em tela
     // é chamado apenas uma vez
-    console.log("Montado"); // mensagens do console.log aparecem no console do navegador
-    this.listarLivros();
-  }
+    console.log('Montado') // mensagens do console.log aparecem no console do navegador
+    this.listarLivros()
+  },
 }
 </script>
 
 <template>
   <main>
     <section>
-      <input
-        type="text"
-        placeholder="Buscar livro"
-        v-model="pesquisa" />
+      <input type="text" placeholder="Buscar livro" v-model="pesquisa" />
     </section>
 
     <h2>Lista de livros ({{ numeroLivros }})</h2>
@@ -67,7 +69,7 @@ export default {
           <img class="cover" :src="livro.cover" alt="Capa do livro" />
           <h3>{{ livro.title }}</h3>
           <p class="author"><strong>Autor:</strong> {{ livro.author }}</p>
-          <p class="description"><strong>Descrição:</strong> {{ livro.description.length > 100 ? livro.description.substring(0, 100) + "..." : livro.description }}</p>
+          <p class="description"><strong>Descrição:</strong> {{ descricaoLivro(livro) }}</p>
         </RouterLink>
       </li>
     </ul>
@@ -100,47 +102,47 @@ h2 {
 }
 
 ul {
+  display: flex;
+  flex-wrap: wrap;
+  gap: 1em;
+
+  li {
     display: flex;
-    flex-wrap: wrap;  
-    gap: 1em;
+    flex-direction: column;
+    border: 1px solid #ccc;
+    border-radius: 0.5em;
+    padding: 1em;
+    max-width: 15em;
+    background-color: white;
+    > a {
+      text-decoration: none;
+      padding: 0.5em 1em;
+      display: flex;
+      flex-direction: column;
 
-    li {
-        display: flex;
-        flex-direction: column;
-        border: 1px solid #ccc;
+      img {
+        max-width: 8em;
+        align-self: center;
         border-radius: 0.5em;
-        padding: 1em;
-        max-width: 15em;
-        background-color: white;
-      > a {
-        text-decoration: none;
-        padding: 0.5em 1em;
-        display: flex;
-        flex-direction: column;
-        
-        img {
-          max-width: 8em;
-          align-self: center;
-          border-radius: 0.5em;
-          margin-block-end: .5em;
-        }
+        margin-block-end: 0.5em;
+      }
 
-        h3 {
-          font-size: 1.2em;
-          margin-block-end: 0.5em;
-        }
+      h3 {
+        font-size: 1.2em;
+        margin-block-end: 0.5em;
+      }
 
-        p {
-          color: #666;
-        }
+      p {
+        color: #666;
+      }
 
-        p.author {
-          margin-block: .5em;
-        }
-        p.description {
-          font-size: 0.8em;
-        }
+      p.author {
+        margin-block: 0.5em;
+      }
+      p.description {
+        font-size: 0.8em;
       }
     }
   }
+}
 </style>
